1a05c50255ae89bdbca7e6555cc0a36d7482ce31
shaarawy6 <139085936+shaarawy6@users.noreply.github.com>
2023-08-30 19:03:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-const navContent = document.querySelectorAll("#nav a");
 const icon = document.querySelectorAll(".icon");
-
-for(let i = 0;i< navContent.length ; i++){
-    navContent[i].addEventListener("mouseover",function(){
-        navContent[i].classList.add("fa-beat");
-    });
-    navContent[i].addEventListener("mouseout",function(){
-        navContent[i].classList.remove("fa-beat");
-    });    
-}
-
 for(let i = 0;i< icon.length ; i++){
     icon[i].addEventListener("mouseover",function(){
         icon[i].classList.add("fa-beat");
